3f44e37b6101d14cf7a81a9b2af0e9ba92f9e645
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-02-22 00:16:36 +0700

    add crud author

--- 4.projects/Final Prjects/newsly/app/Models/Author.php
+++ 4.projects/Final Prjects/newsly/app/Models/Author.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Author extends Model
 {
     use SoftDeletes;
     protected $guarded = [];
+
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function news()
     {
