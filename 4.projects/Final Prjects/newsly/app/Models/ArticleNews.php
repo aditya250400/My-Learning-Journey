fb3c72ba749ed378510da512c92afb402305e349
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-02-21 12:19:53 +0700

    add model,migrations,relationship, and reource filament category

---
+++ 4.projects/Final Prjects/newsly/app/Models/ArticleNews.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ArticleNews extends Model
+{
+    use SoftDeletes;
+    protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class, 'author_id');
+    }
+}
